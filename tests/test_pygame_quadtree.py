5c7dd697c29b73bbc0b4bdbf20e5dc887a85aa59
Bastienlsg <bastien.lassaigne@gmail.com>
2023-11-14 12:15:41 +0100

    test: add the pygame test

--- tests/test_pygame_quadtree.py
+++ tests/test_pygame_quadtree.py
@@ -12,18 +12,21 @@ def setup_pygame():
 
 
 def test_display_quadtree(setup_pygame):
+    # Test searching colors in different pixels
     quadtree = QuadTree(True, False, True, QuadTree(True, True, False, False))
-    quadtree_depth = 2
     screen_size = (750, 750)
 
-    interface = PyGameQuadTree(quadtree, quadtree_depth)
-
-    screen_content_before = pygame.surfarray.array3d(interface._PyGameQuadTree__screen)
+    interface = PyGameQuadTree(quadtree)
 
     interface.display_quadtree(quadtree, 0, 0, *screen_size)
 
-    pygame.time.delay(100)
+    screen_content = pygame.surfarray.array3d(interface._PyGameQuadTree__screen)
 
-    screen_content_after = pygame.surfarray.array3d(interface._PyGameQuadTree__screen)
+    pixel_color_black = screen_content[200, 200]
+    pixel_color_white = screen_content[600, 100]
 
-    assert (screen_content_before == screen_content_after).all()
+    expected_color_black = (14, 17, 17)
+    expected_color_white = (251, 250, 245)
+
+    assert (pixel_color_black == expected_color_black).any()
+    assert (pixel_color_white == expected_color_white).any()
